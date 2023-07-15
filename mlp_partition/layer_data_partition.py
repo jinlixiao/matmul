029c6a3885bcfa9992aa121e42ffae1d6695aa2f
Jinli Xiao <jinli.xiao@nyu.edu>
2023-07-15 18:27:40 -0400

    Add one-layer paritition nn modules

---
+++ mlp_partition/layer_data_partition.py
@@ -0,0 +1,56 @@
+import os
+import time
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+from layers import DataParallelLinearModel
+
+"""
+One Layer MLP Data Parallel
+"""
+
+def process(rank, world_size, data, labels, weights):
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    group = dist.new_group([0, 1])
+
+    # todo: rewrite data spliting in a more general way
+    data_size = data.size(0) // dist.get_world_size()
+    data = data[rank * data_size : (rank + 1) * data_size].to(rank)
+    labels = labels[rank * data_size : (rank + 1) * data_size]
+
+    model = DataParallelLinearModel(weights, group)
+    model = model.to(rank)
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+    # forward pass
+    outputs = model(data)
+    
+    # backward pass
+    labels = labels.to(rank)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+
+    # update parameters
+    optimizer.step()
+    torch.set_printoptions(sci_mode=False, precision=4)
+    print(f"rank{rank}: model now has weights\n{model.linear.weight.data}\n")
+
+if __name__ == "__main__":
+    # communication configurations
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+
+    # model configurations
+    world_size = 2
+    torch.manual_seed(0)
+    data = torch.randn(4, 4)
+    labels = torch.randn(4, 4)
+    weights = torch.randn(4, 4)
+    
+    # run
+    start_time = time.time()
+    mp.spawn(process, args=(world_size, data, labels, weights), nprocs=world_size, join=True)
+    print(f"total time: {time.time() - start_time:.3f} seconds")
+
---
+++ mlp_partition/layers.py
@@ -0,0 +1,61 @@
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+
+
+###############
+# No Parallel
+###############
+
+class LinearModel(nn.Module):
+
+    def __init__(self, weights):
+        super(LinearModel, self).__init__()
+        self.linear = nn.Linear(*weights.t().shape, bias=False)
+        self.linear.weight.data = weights.t().clone()
+
+    def forward(self, input):
+        return self.linear(input)
+
+###############
+# Data Parallel
+###############
+
+class DataParallelLinearModel(nn.Module):
+    """
+    Data parallel linear model
+    """
+
+    def __init__(self, weights, group):
+        super(DataParallelLinearModel, self).__init__()
+        self.linear = nn.Linear(*weights.t().shape, bias=False)
+        self.linear.weight.data = weights.t().clone()
+        self.linear.weight.register_hook(self._all_reduce_hook)
+        self.group = group
+
+    def forward(self, input):
+        return self.linear(input)
+
+    def _all_reduce_hook(self, grad_weight):
+        dist.all_reduce(grad_weight, op=dist.ReduceOp.SUM, group=self.group)
+        return grad_weight / dist.get_world_size()
+
+################
+# Model Parallel
+################
+
+class ModelParallelLinearModel(nn.Module):
+
+    def __init__(self, weights, group):
+        super(ModelParallelLinearModel, self).__init__()
+        self.linear = nn.Linear(*weights.t().shape, bias=False)
+        self.linear.weight.data = weights.t().clone()
+        self.group = group
+
+    def forward(self, input):
+        output_local = self.linear(input)
+        output_list = [torch.empty_like(output_local) for _ in range(dist.get_world_size())]
+        dist.all_gather(output_list, output_local, group=self.group)
+        output_list[dist.get_rank()] = output_local
+        output = torch.cat(output_list, dim=1).contiguous()
+        return output
