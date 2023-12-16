769b2703a969abaa1de048801d78e5ced78a879d
Jinli Xiao <jinli.xiao@nyu.edu>
2023-12-16 08:42:03 -0500

    result for hpc 4gpu pcie

--- overlap/run_scripts.sh
+++ overlap/run_scripts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=1:00:00
-#SBATCH --mem=3000
+#SBATCH --mem=30GB
 #SBATCH -c 4
 #SBATCH --gres=gpu:v100:4
-#SBATCH --output=job_output_%j.txt
+#SBATCH --output=job_output_4gpu_2_%j.txt
 
-# Load Conda and activate the 'pytorch' environment
-conda activate pytorch
+module purge
+source /scratch/jx2076/miniconda3/bin/activate pytorch
 
 # Print the configuration of the nodes
 nvidia-smi topo -m
