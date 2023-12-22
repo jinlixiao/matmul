5c556f3338b77d9107e9fd3b029443cc4d01be05
Jinli Xiao <jinli.xiao@nyu.edu>
2023-12-22 14:43:14 -0500

    [1/2] refactor to add support for selecting models

---
+++ overlap/models.py
@@ -0,0 +1,266 @@
+import torch
+import torch.cuda as cuda
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.distributed as dist
+import utils
+
+_MODEL_REGISTRY = {}
+
+def get_model_class(model_name):
+    try:
+        return _MODEL_REGISTRY[model_name]
+    except KeyError:
+        raise ValueError(f"Unknown model name: {model_name}")
+    
+def register_model(cls):
+    _MODEL_REGISTRY[cls.__name__] = cls
+    return cls
+
+@register_model
+class OverlapTiledAllreduceMLP(nn.Module):
+    """
+    Overlapped version of the two-layer MLP model.
+
+    GEMM + ReLU + GEMM + All-reduce
+
+    Use tiling to overlap (GEMM + ReLU + GEMM) and (All-reduce)
+    """
+
+    def __init__(self, input_size, hidden_size):
+        super(OverlapTiledAllreduceMLP, self).__init__()
+        self.fc1_weight = nn.Parameter(torch.randn(hidden_size, input_size))
+        self.fc1_bias = nn.Parameter(torch.randn(hidden_size))
+        self.fc2_weight = nn.Parameter(torch.randn(input_size, hidden_size))
+        self.fc2_bias = nn.Parameter(torch.randn(input_size))
+
+    def forward(self, x, num_tiles = 1):
+        # Split among the batch dimension
+        input_splits = torch.chunk(x, num_tiles, dim=0)
+        output_ = torch.zeros_like(x)
+        handles = []
+
+        total_duration = 0
+
+        # Launch non-blocking all-reduce operations
+        for i, input_part in enumerate(input_splits):
+            output_part, duration = self._single_forward(input_part)
+            handle = dist.all_reduce(output_part, op=dist.ReduceOp.SUM, group=utils.get_model_parallel_group(), async_op=True)
+            handles.append((handle, output_part, i))
+            total_duration += duration
+
+        # Wait for all operations to complete and gather results
+        for handle, output_part, i in handles:
+            handle.wait()
+            output_[i * output_part.shape[0]:(i + 1) * output_part.shape[0], :] = output_part
+
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total time for single_forward: {total_duration:.04f} milliseconds")
+        return output_
+
+    def _single_forward(self, x):
+        start_event = cuda.Event(enable_timing=True)
+        end_event = cuda.Event(enable_timing=True)
+
+        start_event.record()  # Start timing
+        x = torch.matmul(x, self.fc1_weight.t()) + self.fc1_bias
+        x = F.relu(x)
+        x = torch.matmul(x, self.fc2_weight.t()) + self.fc2_bias
+        end_event.record()  # End timing
+
+        cuda.synchronize()  # Wait for the events to be recorded
+
+        duration = start_event.elapsed_time(end_event)
+        utils.print_rank_0(f"Rank {dist.get_rank()}: single_forward duration: {duration:.04f} milliseconds")
+        return x, duration
+
+@register_model
+class NonOverlapTiledAllreduceMLP(nn.Module):
+    """
+    Non-overlapped version of the two-layer MLP model.
+
+    GEMM + ReLU + GEMM + All-reduce
+    """
+
+    def __init__(self, input_size, hidden_size):
+        super(NonOverlapTiledAllreduceMLP, self).__init__()
+        self.fc1_weight = nn.Parameter(torch.randn(hidden_size, input_size))
+        self.fc1_bias = nn.Parameter(torch.randn(hidden_size))
+        self.fc2_weight = nn.Parameter(torch.randn(input_size, hidden_size))
+        self.fc2_bias = nn.Parameter(torch.randn(input_size))
+
+    def forward(self, x, num_tiles = 1):
+        # Split among the batch dimension
+        input_splits = torch.chunk(x, num_tiles, dim=0)
+        output_ = torch.zeros_like(x)
+
+        total_duration = 0
+        all_reduce_duration = 0
+
+        for i, input_part in enumerate(input_splits):
+            output_part, duration = self._single_forward(input_part)
+            total_duration += duration
+            ar_start_event = cuda.Event(enable_timing=True)
+            ar_end_event = cuda.Event(enable_timing=True)
+            
+            ar_start_event.record()
+            dist.all_reduce(output_part, op=dist.ReduceOp.SUM, group=utils.get_model_parallel_group())
+            ar_end_event.record()
+            output_[i * output_part.shape[0]:(i + 1) * output_part.shape[0], :] = output_part
+            
+            # Calculate all_reduce duration
+            cuda.synchronize()
+            ar_duration = ar_start_event.elapsed_time(ar_end_event)
+            utils.print_rank_0(f"Rank {dist.get_rank()}: All-reduce duration for tile {i}: {ar_duration:.04f} milliseconds")
+            all_reduce_duration += ar_duration
+
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total computation time: {total_duration:.04f} milliseconds")
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total communication time: {all_reduce_duration:.04f} milliseconds")
+        return output_
+
+    def _single_forward(self, x):
+        start_event = cuda.Event(enable_timing=True)
+        end_event = cuda.Event(enable_timing=True)
+
+        start_event.record()  # Start timing
+        x = torch.matmul(x, self.fc1_weight.t()) + self.fc1_bias
+        x = F.relu(x)
+        x = torch.matmul(x, self.fc2_weight.t()) + self.fc2_bias
+        end_event.record()  # End timing
+
+        cuda.synchronize()  # Wait for the events to be recorded
+
+        duration = start_event.elapsed_time(end_event)
+        utils.print_rank_0(f"Rank {dist.get_rank()}: single_forward duration: {duration:.04f} milliseconds")
+        return x, duration
+
+@register_model
+class OverlapTiledAllGatherMLP(nn.Module):
+    """
+    Overlapped version of the two-layer MLP model.
+
+    (GEMM + ReLU + GEMM) overlapping with (All-gather)
+
+    Purpose is to experiment whether all-gather gives the same slowdown
+    to the computation as all-reduce.
+    """
+
+    def __init__(self, input_size, hidden_size):
+        super(OverlapTiledAllGatherMLP, self).__init__()
+        self.fc1_weight = nn.Parameter(torch.randn(hidden_size, input_size))
+        self.fc1_bias = nn.Parameter(torch.randn(hidden_size))
+        self.fc2_weight = nn.Parameter(torch.randn(input_size, hidden_size))
+        self.fc2_bias = nn.Parameter(torch.randn(input_size))
+
+    def forward(self, x, num_tiles = 1):
+        # Split among the batch dimension
+        input_splits = torch.chunk(x, num_tiles, dim=0)
+        output_list = []
+        total_duration = 0
+
+        # Launch non-blocking all-gather operations
+        for i, input_part in enumerate(input_splits):
+            output_part, duration = self._single_forward(input_part)
+            gathered_outputs = [torch.zeros_like(output_part) for _ in range(dist.get_world_size())]
+            handle = dist.all_gather(gathered_outputs, output_part, group=utils.get_model_parallel_group(), async_op=True)
+            output_list.append((handle, gathered_outputs))
+            total_duration += duration
+
+        # Wait for all operations to complete
+        for handle, _ in output_list:
+            handle.wait()
+
+        # Correctly combine the gathered results
+        outputs_combined = []
+        for _, gathered in output_list:
+            for tensor in gathered:
+                outputs_combined.append(tensor)
+
+        output_ = torch.cat(outputs_combined, dim=0)
+
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total time for single_forward: {total_duration:.04f} milliseconds")
+        return x
+
+    def _single_forward(self, x):
+        start_event = cuda.Event(enable_timing=True)
+        end_event = cuda.Event(enable_timing=True)
+
+        start_event.record()  # Start timing
+        x = torch.matmul(x, self.fc1_weight.t()) + self.fc1_bias
+        x = F.relu(x)
+        x = torch.matmul(x, self.fc2_weight.t()) + self.fc2_bias
+        end_event.record()  # End timing
+
+        cuda.synchronize()  # Wait for the events to be recorded
+
+        duration = start_event.elapsed_time(end_event)
+        utils.print_rank_0(f"Rank {dist.get_rank()}: single_forward duration: {duration:.04f} milliseconds")
+        return x, duration
+    
+@register_model
+class NonoverlapNontiledMLP(nn.Module):
+    """
+    Non-overlapped version of the two-layer MLP model.
+
+    GEMM + ReLU + GEMM + Reduce-scatter + All-gather
+    """
+
+    def __init__(self, input_size, hidden_size):
+        super(NonoverlapNontiledMLP, self).__init__()
+        self.fc1_weight = nn.Parameter(torch.randn(hidden_size, input_size))
+        self.fc1_bias = nn.Parameter(torch.randn(hidden_size))
+        self.fc2_weight = nn.Parameter(torch.randn(input_size, hidden_size))
+        self.fc2_bias = nn.Parameter(torch.randn(input_size))
+
+    def forward(self, x):
+        # Start timing for the forward pass
+        start_event = cuda.Event(enable_timing=True)
+        end_event = cuda.Event(enable_timing=True)
+        start_event.record()
+
+        # First layer operations
+        x = torch.matmul(x, self.fc1_weight.t()) + self.fc1_bias
+        x = F.relu(x)
+
+        # Second layer operations
+        x = torch.matmul(x, self.fc2_weight.t()) + self.fc2_bias
+
+        # End timing for matrix multiplication
+        end_event.record()
+        cuda.synchronize()
+        single_forward_duration = start_event.elapsed_time(end_event)
+
+        # Determine the size for each split
+        world_size = dist.get_world_size()
+        split_size = x.size(0) // world_size
+        assert x.size(0) % world_size == 0
+
+        # Split the output for reduce-scatter operation
+        outputs_split = list(torch.split(x, split_size, dim=0))
+
+        # Record time for reduce-scatter operation
+        start_event.record()
+        scatter_output = torch.zeros_like(outputs_split[dist.get_rank()])
+        dist.reduce_scatter(scatter_output, outputs_split, group=utils.get_model_parallel_group())
+        end_event.record()
+
+        # Wait for the event to be recorded and calculate duration
+        cuda.synchronize()
+        reduce_scatter_duration = start_event.elapsed_time(end_event)
+
+        # All-gather operation
+        start_event.record()
+        gather_list = [torch.zeros_like(scatter_output) for _ in range(world_size)]
+        dist.all_gather(gather_list, scatter_output, group=utils.get_model_parallel_group())
+        end_event.record()
+
+        # Wait for the event to be recorded and calculate duration
+        cuda.synchronize()
+        all_gather_duration = start_event.elapsed_time(end_event)
+
+        # Concatenate the gathered outputs
+        output = torch.cat(gather_list, dim=0)
+
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total time for matrix multiplications: {single_forward_duration:.04f} milliseconds")
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total time for reduce_scatter: {reduce_scatter_duration:.04f} milliseconds")
+        utils.print_rank_0(f"Rank {dist.get_rank()}: Total time for all_gather: {all_gather_duration:.04f} milliseconds")
+        return output
--- overlap/tiled_mlp.py
+++ overlap/tiled_mlp.py
@@ -1,10 +1,8 @@
 import argparse
 import torch
 import torch.cuda as cuda
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.distributed as dist
 import torch.multiprocessing as mp
+import models
 import utils
 
 NNODES = 1                 # Number of nodes
@@ -16,56 +14,11 @@ parser = argparse.ArgumentParser(description='Tiled Matrix Multiplication')
 parser.add_argument('--num_tiles', type=int, default=1, help='Number of tiles to split the input into')
 parser.add_argument('--num_iterations', type=int, default=10, help='Number of iterations to run')
 parser.add_argument('--num_devices', type=int, default=torch.cuda.device_count(), help='Number of devices to use')
+parser.add_argument('--model_name', type=str, default='OverlapTiledAllreduceMLP', help='Name of the model')
 args = parser.parse_args()
 
 MODEL_PARALLEL_SIZE = args.num_devices    # Number of GPUs per node
-
-class TwoLayerMLP(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(TwoLayerMLP, self).__init__()
-        self.fc1_weight = nn.Parameter(torch.randn(hidden_size, input_size))
-        self.fc1_bias = nn.Parameter(torch.randn(hidden_size))
-        self.fc2_weight = nn.Parameter(torch.randn(input_size, hidden_size))
-        self.fc2_bias = nn.Parameter(torch.randn(input_size))
-
-    def forward(self, x, num_tiles):
-        # Split among the batch dimension
-        input_splits = torch.chunk(x, num_tiles, dim=0)
-        output_ = torch.zeros_like(x)
-        handles = []
-
-        total_duration = 0
-
-        # Launch non-blocking all-reduce operations
-        for i, input_part in enumerate(input_splits):
-            output_part, duration = self.single_forward(input_part)
-            handle = dist.all_reduce(output_part, op=dist.ReduceOp.SUM, group=utils.get_model_parallel_group(), async_op=True)
-            handles.append((handle, output_part, i))
-            total_duration += duration
-
-        # Wait for all operations to complete and gather results
-        for handle, output_part, i in handles:
-            handle.wait()
-            output_[i * output_part.shape[0]:(i + 1) * output_part.shape[0], :] = output_part
-
-        utils.print_rank_0(f"Rank {dist.get_rank()}: Total time for single_forward: {total_duration:.04f} milliseconds")
-        return output_
-
-    def single_forward(self, x):
-        start_event = cuda.Event(enable_timing=True)
-        end_event = cuda.Event(enable_timing=True)
-
-        start_event.record()  # Start timing
-        x = torch.matmul(x, self.fc1_weight.t()) + self.fc1_bias
-        x = F.relu(x)
-        x = torch.matmul(x, self.fc2_weight.t()) + self.fc2_bias
-        end_event.record()  # End timing
-
-        cuda.synchronize()  # Wait for the events to be recorded
-
-        duration = start_event.elapsed_time(end_event)
-        utils.print_rank_0(f"Rank {dist.get_rank()}: single_forward duration: {duration:.04f} milliseconds")
-        return x, duration
+USE_TILES = args.model_name in ['OverlapTiledAllreduceMLP', 'OverlapTiledAllgatherMLP', 'OverlapTiledReduceScatterMLP']
 
 def run(rank, world_size):
     utils.parallel_init(rank, world_size, run_with_cpu=RUN_WITH_CPU)
@@ -73,11 +26,12 @@ def run(rank, world_size):
     assert model_parallel_group is not None
 
     # Create the MLP model
+    model = models.get_model_class(args.model_name)
     if RUN_WITH_CPU:
-        mlp = TwoLayerMLP(H, 4 * H // MODEL_PARALLEL_SIZE)
+        mlp = model(H, 4 * H // MODEL_PARALLEL_SIZE)
         input_ = torch.randn(B, L, H)
     else:
-        mlp = TwoLayerMLP(H, 4 * H // MODEL_PARALLEL_SIZE).cuda(rank)
+        mlp = model(H, 4 * H // MODEL_PARALLEL_SIZE).cuda(rank)
         input_ = torch.randn(B, L, H).cuda(rank)
 
     # Run the model
@@ -89,7 +43,10 @@ def run(rank, world_size):
             end_event = cuda.Event(enable_timing=True)
 
             start_event.record()  # Start timing
-            input_ = mlp(input_, num_tiles=args.num_tiles)
+            if USE_TILES:
+                input_ = mlp(input_, num_tiles=args.num_tiles)
+            else:
+                input_ = mlp(input_)
             end_event.record()  # End timing
             cuda.synchronize()  # Wait for the events to be recorded
             
@@ -104,6 +61,9 @@ def run(rank, world_size):
     utils.print_rank_0(f"Median time for tiled matrix multiplication: {sorted(running_time)[len(running_time) // 2]:.04f} milliseconds")
 
 if __name__ == "__main__":
-    print(f"Running on {MODEL_PARALLEL_SIZE} GPUs per node, tile size {args.num_tiles}")
+    if USE_TILES:
+        print(f"Running {args.model_name} on {MODEL_PARALLEL_SIZE} GPUs per node, tile size {args.num_tiles}")
+    else:
+        print(f"Running {args.model_name} on {MODEL_PARALLEL_SIZE} GPUs per node")
     world_size = NNODES * MODEL_PARALLEL_SIZE
     mp.spawn(run, args=(world_size,), nprocs=world_size, join=True)
